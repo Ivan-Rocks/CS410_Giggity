455f8fb3974ba703499dc131295c58feef146d29
jialinh4 <jialinh4@illinois.edu>
2023-11-09 18:05:04 -0600

    server created

--- server/app/models/nlp_model.py
+++ server/app/models/nlp_model.py
@@ -1,9 +1,7 @@
 import spacy
 
-# Load the spaCy model
 nlp = spacy.load("en_core_web_sm")
 
 def tokenize_text(text):
-    # Tokenize text using spaCy
     doc = nlp(text)
     return [token.text for token in doc]
--- server/server.py
+++ server/server.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 from app.api.nlp import nlp_blueprint
 from app.api.summarizer import summarizer_blueprint
 from app.api.sentiment import sentiment_blueprint
 from app.api.search import search_blueprint
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS
 
 # Register blueprints
 app.register_blueprint(nlp_blueprint)
