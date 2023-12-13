c1b7b26876f020a5fb78bbc1c53f18bd3854b834
jialinh4 <jialinh4@illinois.edu>
2023-12-13 17:43:09 -0600

    Update popup.js

--- TextAnalyzerExtension/popup.js
+++ TextAnalyzerExtension/popup.js
@@ -1,37 +1,53 @@
-// not clean
+// Function to call OpenAI API for summarizing or sentiment analysis
 function callOpenAI(text, task) {
     const prompt = task === 'summarize' ? `Summarize this text: ${text}` : `What is the sentiment of this text: ${text}`;
     return fetch('https://api.openai.com/v1/engines/davinci/completions', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
-            'Authorization': 'Bearer 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' // Insert your API key here
+            'Authorization': 'Bearer YOUR_API_KEY' // Update with the correct API key
         },
-        body: JSON.stringify({
-            prompt: prompt,
-            max_tokens: 200
-        })
+        body: JSON.stringify({ prompt: prompt, max_tokens: 200 })
     })
-    .then(response => response.json())
-    .then(data => data.choices[0].text.trim());
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`API call failed: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then(data => {
+        if (!data.choices || data.choices.length === 0 || !data.choices[0].text) {
+            throw new Error('Invalid response structure from API');
+        }
+        return data.choices[0].text.trim();
+    })
+    .catch(error => {
+        console.error('Error in callOpenAI:', error);
+        throw error; // Rethrow to handle in calling function
+    });
 }
-// clean
+
+// Function to display text and handle action type
 function displayText(action) {
-    document.getElementById('result').textContent = 'before'; // Initial text
+    document.getElementById('result').textContent = 'Loading...'; // Initial text
 
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {method: "getText"}, function(response) {
             if (response && response.data) {
-                //Sdocument.getElementById('result').textContent = "Giggity";
                 window.documentText = response.data.toLowerCase().match(/\b(\w+)\b/g);
-                document.getElementById('result').textContent = "Text Successfully Retrieved";
+                if (action === 'sentiment') {
+                    analyzeSentiment(response.data);
+                } else {
+                    document.getElementById('result').textContent = "Text Successfully Retrieved";
+                }
             } else {
                 document.getElementById('result').textContent = 'No text found or content script not responding';
             }
         });
     });
 }
-// clean
+
+// Function to calculate Term Frequency (TF)
 function calculateTF(words) {
     const wordCount = words.length;
     const wordFreq = {};
@@ -40,56 +56,86 @@ function calculateTF(words) {
     });
     for (let word in wordFreq) {
         wordFreq[word] = wordFreq[word] / wordCount;
-        //console.log(word + wordFreq[word]);
     }
     return wordFreq;
 }
 
+// Function to fetch and calculate Inverse Document Frequency (IDF)
 async function calculateIDF() {
     try {
         const response = await fetch('IDF.json'); // URL where the JSON file is hosted
         const idfData = await response.json();
-        window.idf = idfData; // Now it's safe to assign to a global variable
-        // You can also return 'idfData' here
+        window.idf = idfData;
         return idfData;
     } catch (error) {
         console.error('Error fetching the JSON file:', error);
     }
 }
-// clean
+
+// Function to calculate TF-IDF
 function calculateTFIDF(TF, IDF) {
     let tfIdfScores = [];
-    const defaultIdf = 4; // Or calculate a dynamic default IDF value
+    const defaultIdf = 4; // Default IDF value
 
-    // Calculate TF-IDF score for each term and push to array
     for (const term in TF) {
         const idf = term in IDF ? IDF[term] : defaultIdf;
         tfIdfScores.push({ term: term, score: TF[term] * idf });
     }
-    // Sort the array by TF-IDF score in descending order
+
     tfIdfScores.sort((a, b) => b.score - a.score);
     return tfIdfScores;
 }
-// clean
+
+// Function to summarize text
 async function summarizeText(action) {
     const tf = calculateTF(window.documentText);
-    //IDF part
     const idf = await calculateIDF();
     window.TFIDF = calculateTFIDF(tf, idf);
     const topTfIdfScores = TFIDF.slice(0, 20);
     let textContent = '';
     for (const item of topTfIdfScores) {
-        // Assuming each item has 'term' and 'score' properties
-        textContent += `${item.term}\n`
-        //textContent += `Term: ${item.term}, Score: ${item.score}\n`;
+        textContent += `${item.term}\n`;
     }
     document.getElementById('result').textContent = textContent;
-    //document.getElementById('result').textContent = window.tf;
 }
 
-// right click on extension icon and click on inspect popup, logs show up there
-// idf corpus online: http://archives.textfiles.com/stories.zip
+// New function to analyze sentiment
+const positiveKeywords = ['happy', 'great', 'joy', 'positive', 'successful', 'good', 'love', 'excellent', 'fortunate', 'correct', 'superior'];
+const negativeKeywords = ['sad', 'bad', 'trouble', 'negative', 'fail', 'wrong', 'poor', 'hate', 'terrible', 'awful', 'inferior'];
 
+async function analyzeSentiment(text) {
+    let positiveCount = 0;
+    let negativeCount = 0;
+    
+    const words = text.toLowerCase().match(/\b(\w+)\b/g); // Extract words from text
+
+    words.forEach(word => {
+        if (positiveKeywords.includes(word)) positiveCount++;
+        if (negativeKeywords.includes(word)) negativeCount++;
+    });
+
+    let sentimentResult = 'Neutral';
+    if (positiveCount > negativeCount) {
+        sentimentResult = 'Positive';
+    } else if (negativeCount > positiveCount) {
+        sentimentResult = 'Negative';
+    }
+
+    document.getElementById('result').textContent = `Sentiment: ${sentimentResult}`;
+}
+
+// Event listeners for buttons
 document.getElementById('runpageBtn').addEventListener('click', () => displayText('runpage'));
 document.getElementById('summarizeBtn').addEventListener('click', () => summarizeText('summarize'));
-document.getElementById('sentimentBtn').addEventListener('click', () => displayText('sentiment'));
+document.getElementById('sentimentBtn').addEventListener('click', () => {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {method: "getText"}, function(response) {
+            if (response && response.data) {
+                analyzeSentiment(response.data);
+            } else {
+                document.getElementById('result').textContent = 'No text found or content script not responding';
+            }
+        });
+    });
+});
+
